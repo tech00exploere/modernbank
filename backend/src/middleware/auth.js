e7b0c0e97631eb14da94238e3eddc3d2841c8c1e
Priyanshu <priyanshu2507.rjs@gmail.com>
2026-05-21 22:27:42 +0530

    Update auth.js

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");//auth establishment
 
 const authRequired = (req, res, next) => {
   if (req.session && req.session.user) {
